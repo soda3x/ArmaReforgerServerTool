64083220422aee046bae256d94d1e042219bdfb0
Bradley Newman <bnewman227@gmail.com>
2025-10-06 21:22:38 +1030

    Progress on Save Manager

---
+++ ArmaReforgerServerTool/Forms/SaveSelector.cs
@@ -0,0 +1,132 @@
+/******************************************************************************
+ * File Name:    SaveSelector.cs
+ * Project:      Longbow
+ * Description:  This is the Save Selector Form
+ * 
+ * Author:       Bradley Newman
+ ******************************************************************************/
+
+using ReforgerServerApp;
+using ReforgerServerApp.Managers;
+using ReforgerServerApp.Utils;
+
+namespace Longbow.Forms
+{
+  public partial class SaveSelector : Form
+  {
+    Dictionary<string, string> m_savedGames;
+    public SaveSelector()
+    {
+      InitializeComponent();
+      CreateToolTips();
+      RefreshSavedGamesList();
+    }
+
+    private void RefreshSavedGamesList()
+    {
+      m_savedGames = FileIOManager.GetInstance().GetSavedGames();
+      savedGamesList.Items.Clear();
+      foreach (string save in m_savedGames.Keys)
+      {
+        savedGamesList.Items.Add(save);
+      }
+    }
+
+    private void CreateToolTips()
+    {
+      ToolTip keepToolTip = new();
+      keepToolTip.SetToolTip(keepPermanentlyBtn, "Keep Selected Autosave");
+
+      ToolTip renameToolTip = new();
+      renameToolTip.SetToolTip(renameSaveBtn, "Rename Selected Save");
+
+      ToolTip deleteToolTip = new();
+      deleteToolTip.SetToolTip(deleteSaveBtn, "Delete Selected Save");
+
+      ToolTip useSaveToolTip = new();
+      useSaveToolTip.SetToolTip(loadSelectedSaveBtn, "Load Selected Save");
+    }
+
+    /// <summary>
+    /// Toggle Button Enabled states based on logic required
+    /// </summary>
+    /// <param name="selectedItem"></param>
+    private void ToggleButtonStates(object? selectedItem)
+    {
+      renameSaveBtn.Enabled = selectedItem != null;
+      loadSelectedSaveBtn.Enabled = selectedItem != null;
+      renameSaveBtn.Enabled = selectedItem != null;
+      deleteSaveBtn.Enabled = selectedItem != null && !selectedItem.Equals(string.Empty);
+      clearSaveBtn.Enabled = ConfigurationManager.GetInstance().usingSave;
+
+      if (selectedItem != null && selectedItem is string saveStr)
+      {
+        keepPermanentlyBtn.Enabled = saveStr.Contains(".auto");
+      }
+    }
+
+    private void SavedGamesListSelectedIndexChanged(object sender, EventArgs e)
+    {
+      ToggleButtonStates(savedGamesList.SelectedItem);
+    }
+
+    private void KeepBtnPressed(object sender, EventArgs e)
+    {
+      string origNameKey = (string) savedGamesList.SelectedItem;
+      string origName = m_savedGames[origNameKey];
+      string newName = origName.Replace(".auto", "");
+      FileIOManager.GetInstance().RenameFile(origName, newName);
+      ToggleButtonStates(savedGamesList.SelectedItem);
+      RefreshSavedGamesList();
+    }
+
+    private void DeleteBtnPressed(object sender, EventArgs e)
+    {
+      string selectedSave = (string) savedGamesList.SelectedItem!;
+      bool confirm = Utilities.DisplayConfirmationMessage($"You are about to delete your saved game '{selectedSave}', are you sure you would like to do this?", true);
+      if (confirm)
+      {
+        FileIOManager.DeleteFile(m_savedGames[selectedSave]);
+        ToggleButtonStates(savedGamesList.SelectedItem);
+        RefreshSavedGamesList();
+      }
+    }
+
+    private void RenameBtnPressed(object sender, EventArgs e)
+    {
+      string origName = (string) savedGamesList.SelectedItem!;
+      RenameForm rf = new(origName);
+      DialogResult result = rf.ShowDialog();
+      if (result == DialogResult.OK)
+      {
+        string origPath = m_savedGames[origName];
+        string newPath = $"{FileIOManager.GetInstance().GetSavesPath()}\\{rf.GetResultingRename()}.json";
+        FileIOManager.GetInstance().RenameFile(origPath, newPath);
+      }
+      RefreshSavedGamesList();
+    }
+
+    private void LoadBtnPressed(object sender, EventArgs e)
+    {
+      ConfigurationManager.GetInstance().save = (string) savedGamesList.SelectedItem!;
+      ConfigurationManager.GetInstance().usingSave = true;
+      loadedSaveLabel.Text = $"Loaded save: {ConfigurationManager.GetInstance().save}";
+      ToggleButtonStates(savedGamesList.SelectedItem);
+    }
+
+    private void LatestBtnPressed(object sender, EventArgs e)
+    {
+      ConfigurationManager.GetInstance().save = ".LatestSave";
+      ConfigurationManager.GetInstance().usingSave = true;
+      loadedSaveLabel.Text = $"Using latest save.";
+      ToggleButtonStates(savedGamesList.SelectedItem);
+    }
+
+    private void ClearSaveBtnPressed(object sender, EventArgs e)
+    {
+      ConfigurationManager.GetInstance().usingSave = false;
+      loadedSaveLabel.Text = "No save loaded. Your server will not use a save.";
+      ToggleButtonStates(savedGamesList.SelectedItem);
+    }
+  }
+}
