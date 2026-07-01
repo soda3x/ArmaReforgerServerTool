0d8e5309b093860314e774937ea7051a4d514061
Bradley Newman <bnewman227@gmail.com>
2026-07-01 19:51:49 +0930

    Fix focus and scroll issues with the FluentTimePicker

--- ArmaReforgerServerTool/Components/ui/FluentTimePicker.cs
+++ ArmaReforgerServerTool/Components/ui/FluentTimePicker.cs
@@ -8,24 +8,23 @@ namespace Longbow.Components.ui
 {
   public class FluentTimePicker : Control
   {
-    private int borderRadius = 8;
-    private Color borderColor = Color.FromArgb(120, 120, 120);
-    private Color focusedBorderColor = Color.FromArgb(0, 120, 212); // Windows 11 Blue
-    private Color fieldBackColor = SystemColors.Window;
+    [System.Runtime.InteropServices.DllImport("user32.dll")]
+    private static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
 
-    // State
-    private bool isHovered = false;
-    private bool isFocused = false;
-    private DateTime _value = DateTime.Now; // Default to now
-    private DateTime _minDate = DateTime.Now;
-    private DateTime _maxDate = DateTime.Now;
-    private string _customFormat = "hh\\:mm"; // 24-hour format
-
-    // Popup components
-    private ToolStripDropDown popup;
-    private TableLayoutPanel popupContainer;
-    private ListBox lstHours;
-    private ListBox lstMinutes;
+    private int m_borderRadius = 8;
+    private Color m_borderColour = Color.FromArgb(120, 120, 120);
+    private Color m_focusedBorderColour = Color.FromArgb(0, 120, 212); // Windows 11 Blue
+    private Color m_fieldBackColour = SystemColors.Window;
+    private bool m_isHovered = false;
+    private bool m_isFocused = false;
+    private DateTime m_value = DateTime.Now; // Default to now
+    private DateTime m_minDate = DateTime.Now;
+    private DateTime m_maxDate = DateTime.Now;
+    private string m_timeFormat = "hh\\:mm"; // 24-hour format
+    private ToolStripDropDown m_popup;
+    private TableLayoutPanel m_popupContainer;
+    private ListBox m_lstHours;
+    private ListBox m_lstMinutes;
 
     public event EventHandler ValueChanged;
 
@@ -46,49 +45,52 @@ namespace Longbow.Components.ui
 
     private void InitializePopup()
     {
-      popupContainer = new TableLayoutPanel();
-      popupContainer.ColumnCount = 2;
-      popupContainer.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-      popupContainer.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-      popupContainer.RowCount = 1;
-      popupContainer.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-      popupContainer.Size = new Size(160, 220); // Height of the dropdown flyout
-      popupContainer.Margin = Padding.Empty;
-      popupContainer.Padding = new Padding(2);
+      m_popupContainer = new TableLayoutPanel();
+      m_popupContainer.ColumnCount = 2;
+      m_popupContainer.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+      m_popupContainer.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+      m_popupContainer.RowCount = 1;
+      m_popupContainer.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+      m_popupContainer.Size = new Size(160, 220); // Height of the dropdown flyout
+      m_popupContainer.Margin = Padding.Empty;
+      m_popupContainer.Padding = new Padding(2);
 
-      lstHours = CreatePopupList();
+      m_lstHours = CreatePopupList();
       for (int i = 0; i <= 23; i++)
-        lstHours.Items.Add(i.ToString("D2"));
+        m_lstHours.Items.Add(i.ToString("D2"));
 
-      lstMinutes = CreatePopupList();
+      m_lstMinutes = CreatePopupList();
       for (int i = 0; i <= 59; i++)
-        lstMinutes.Items.Add(i.ToString("D2"));
+        m_lstMinutes.Items.Add(i.ToString("D2"));
 
-      popupContainer.Controls.Add(lstHours, 0, 0);
-      popupContainer.Controls.Add(lstMinutes, 1, 0);
+      m_lstHours.MouseEnter += (s, e) => m_lstHours.Focus();
+      m_lstMinutes.MouseEnter += (s, e) => m_lstMinutes.Focus();
+
+      m_popupContainer.Controls.Add(m_lstHours, 0, 0);
+      m_popupContainer.Controls.Add(m_lstMinutes, 1, 0);
 
       // Update the Time value when the user clicks an item in the popup
       EventHandler listSelectionChanged = (s, e) =>
         {
-          if (lstHours.SelectedIndex >= 0 && lstMinutes.SelectedIndex >= 0)
+          if (m_lstHours.SelectedIndex >= 0 && m_lstMinutes.SelectedIndex >= 0)
           {
             // Preserve the date, overwrite the time
-            this.Value = new DateTime(_value.Year, _value.Month, _value.Day, lstHours.SelectedIndex, lstMinutes.SelectedIndex, 0);
+            this.Value = new DateTime(m_value.Year, m_value.Month, m_value.Day, m_lstHours.SelectedIndex, m_lstMinutes.SelectedIndex, 0);
           }
         };
-      lstHours.SelectedIndexChanged += listSelectionChanged;
-      lstMinutes.SelectedIndexChanged += listSelectionChanged;
+      m_lstHours.SelectedIndexChanged += listSelectionChanged;
+      m_lstMinutes.SelectedIndexChanged += listSelectionChanged;
 
-      ToolStripControlHost host = new ToolStripControlHost(popupContainer);
+      ToolStripControlHost host = new ToolStripControlHost(m_popupContainer);
       host.Margin = Padding.Empty;
       host.Padding = Padding.Empty;
       host.AutoSize = false;
-      host.Size = popupContainer.Size;
+      host.Size = m_popupContainer.Size;
 
-      popup = new ToolStripDropDown();
-      popup.Padding = new Padding(1);
-      popup.Items.Add(host);
-      popup.DropShadowEnabled = true;
+      m_popup = new ToolStripDropDown();
+      m_popup.Padding = new Padding(1);
+      m_popup.Items.Add(host);
+      m_popup.DropShadowEnabled = true;
     }
 
     private ListBox CreatePopupList()
@@ -109,7 +111,7 @@ namespace Longbow.Components.ui
 
         bool isSelected = (e.State & DrawItemState.Selected) == DrawItemState.Selected;
 
-        using (SolidBrush bgBrush = new SolidBrush(fieldBackColor))
+        using (SolidBrush bgBrush = new SolidBrush(m_fieldBackColour))
           g.FillRectangle(bgBrush, e.Bounds);
 
         if (isSelected)
@@ -133,12 +135,12 @@ namespace Longbow.Components.ui
     [Category("Data")]
     public DateTime Value
     {
-      get => _value;
+      get => m_value;
       set
       {
-        if (_value != value)
+        if (m_value != value)
         {
-          _value = value;
+          m_value = value;
           this.Invalidate();
           ValueChanged?.Invoke(this, EventArgs.Empty);
         }
@@ -147,34 +149,34 @@ namespace Longbow.Components.ui
 
     public DateTime MinDate
     {
-      get => _minDate;
-      set => _minDate = value;
+      get => m_minDate;
+      set => m_minDate = value;
     }
 
     public DateTime MaxDate
     {
-      get => _maxDate;
-      set => _maxDate = value;
+      get => m_maxDate;
+      set => m_maxDate = value;
     }
 
     [Category("Appearance")]
     public string CustomFormat
     {
-      get => _customFormat;
-      set { _customFormat = value; this.Invalidate(); }
+      get => m_timeFormat;
+      set { m_timeFormat = value; this.Invalidate(); }
     }
 
     [Category("Appearance")]
     public Color FieldBackColor
     {
-      get => fieldBackColor;
-      set { fieldBackColor = value; this.Invalidate(); }
+      get => m_fieldBackColour;
+      set { m_fieldBackColour = value; this.Invalidate(); }
     }
 
-    protected override void OnMouseEnter(EventArgs e) { isHovered = true; this.Invalidate(); base.OnMouseEnter(e); }
-    protected override void OnMouseLeave(EventArgs e) { isHovered = false; this.Invalidate(); base.OnMouseLeave(e); }
-    protected override void OnEnter(EventArgs e) { isFocused = true; this.Invalidate(); base.OnEnter(e); }
-    protected override void OnLeave(EventArgs e) { isFocused = false; this.Invalidate(); base.OnLeave(e); }
+    protected override void OnMouseEnter(EventArgs e) { m_isHovered = true; this.Invalidate(); base.OnMouseEnter(e); }
+    protected override void OnMouseLeave(EventArgs e) { m_isHovered = false; this.Invalidate(); base.OnMouseLeave(e); }
+    protected override void OnEnter(EventArgs e) { m_isFocused = true; this.Invalidate(); base.OnEnter(e); }
+    protected override void OnLeave(EventArgs e) { m_isFocused = false; this.Invalidate(); base.OnLeave(e); }
     protected override void OnEnabledChanged(EventArgs e) { base.OnEnabledChanged(e); this.Invalidate(); }
 
     protected override void OnMouseClick(MouseEventArgs e)
@@ -184,18 +186,50 @@ namespace Longbow.Components.ui
       {
         this.Focus();
 
-        popupContainer.BackColor = fieldBackColor;
-        lstHours.BackColor = fieldBackColor;
-        lstMinutes.BackColor = fieldBackColor;
-        popup.BackColor = borderColor;
+        m_popupContainer.BackColor = m_fieldBackColour;
+        m_lstHours.BackColor = m_fieldBackColour;
+        m_lstMinutes.BackColor = m_fieldBackColour;
+        m_popup.BackColor = m_borderColour;
 
-        lstHours.SelectedIndex = _value.Hour;
-        lstMinutes.SelectedIndex = _value.Minute;
+        m_lstHours.SelectedIndex = m_value.Hour;
+        m_lstMinutes.SelectedIndex = m_value.Minute;
 
-        lstHours.TopIndex = Math.Max(0, _value.Hour - 2);
-        lstMinutes.TopIndex = Math.Max(0, _value.Minute - 2);
+        m_lstHours.TopIndex = Math.Max(0, m_value.Hour - 2);
+        m_lstMinutes.TopIndex = Math.Max(0, m_value.Minute - 2);
 
-        popup.Show(this, new Point(0, this.Height + 2));
+        m_popup.Show(this, new Point(0, this.Height + 2));
+      }
+    }
+
+    protected override void OnMouseWheel(MouseEventArgs e)
+    {
+      if (m_popup != null && m_popup.Visible)
+      {
+        // Determine which ListBox the mouse is actually over
+        Point mousePos = Cursor.Position;
+
+        // Find which list the mouse is over
+        ListBox target = null;
+        if (m_lstHours.Bounds.Contains(m_popupContainer.PointToClient(m_popup.PointToClient(mousePos))))
+          target = m_lstHours;
+        else if (m_lstMinutes.Bounds.Contains(m_popupContainer.PointToClient(m_popup.PointToClient(mousePos))))
+          target = m_lstMinutes;
+
+        if (target != null)
+        {
+          // The Win32 message WM_MOUSEWHEEL is 0x020A, use SendMessage to force the ListBox to process the scroll
+          const int WM_MOUSEWHEEL = 0x020A;
+          // Create the WParam containing the wheel delta (e.Delta)
+          IntPtr wParam = (IntPtr)((e.Delta << 16) | (0x0000FFFF & (int)0));
+          // Send the message directly to the ListBox handle
+          SendMessage(target.Handle, WM_MOUSEWHEEL, wParam, IntPtr.Zero);
+        }
+        // Stop the form from attempting to scroll
+        ((HandledMouseEventArgs) e).Handled = true;
+      }
+      else
+      {
+        base.OnMouseWheel(e);
       }
     }
 
@@ -205,24 +239,24 @@ namespace Longbow.Components.ui
       Graphics g = e.Graphics;
       g.SmoothingMode = SmoothingMode.AntiAlias;
 
-      Color currentBackColor = fieldBackColor;
-      Color currentBorderColor = isFocused ? focusedBorderColor : borderColor;
+      Color currentBackColor = m_fieldBackColour;
+      Color currentBorderColor = m_isFocused ? m_focusedBorderColour : m_borderColour;
       Color currentTextColor = this.ForeColor;
 
       if (!this.Enabled)
       {
-        currentBackColor = Color.FromArgb(Math.Max(0, fieldBackColor.R - 15), Math.Max(0, fieldBackColor.G - 15), Math.Max(0, fieldBackColor.B - 15));
+        currentBackColor = Color.FromArgb(Math.Max(0, m_fieldBackColour.R - 15), Math.Max(0, m_fieldBackColour.G - 15), Math.Max(0, m_fieldBackColour.B - 15));
         currentBorderColor = Color.FromArgb(100, 150, 150, 150);
         currentTextColor = Color.FromArgb(150, 150, 150);
       }
-      else if (isHovered && !isFocused)
+      else if (m_isHovered && !m_isFocused)
       {
-        currentBackColor = Color.FromArgb(Math.Max(0, fieldBackColor.R - 10), Math.Max(0, fieldBackColor.G - 10), Math.Max(0, fieldBackColor.B - 10));
+        currentBackColor = Color.FromArgb(Math.Max(0, m_fieldBackColour.R - 10), Math.Max(0, m_fieldBackColour.G - 10), Math.Max(0, m_fieldBackColour.B - 10));
       }
 
-      float borderThickness = isFocused ? 2f : 1.5f;
+      float borderThickness = m_isFocused ? 2f : 1.5f;
 
-      using (GraphicsPath path = GetRoundedRect(new Rectangle(0, 0, this.Width - 1, this.Height - 1), borderRadius))
+      using (GraphicsPath path = GetRoundedRect(new Rectangle(0, 0, this.Width - 1, this.Height - 1), m_borderRadius))
       {
         using (SolidBrush brush = new SolidBrush(currentBackColor))
           g.FillPath(brush, path);
@@ -230,15 +264,15 @@ namespace Longbow.Components.ui
         using (Pen pen = new Pen(currentBorderColor, borderThickness))
         {
           g.DrawPath(pen, path);
-          if (isFocused)
+          if (m_isFocused)
           {
-            using (Pen thickPen = new Pen(focusedBorderColor, 3f))
-              g.DrawLine(thickPen, borderRadius, this.Height - 2, this.Width - borderRadius, this.Height - 2);
+            using (Pen thickPen = new Pen(m_focusedBorderColour, 3f))
+              g.DrawLine(thickPen, m_borderRadius, this.Height - 2, this.Width - m_borderRadius, this.Height - 2);
           }
         }
       }
 
-      string displayText = _value.ToString(_customFormat);
+      string displayText = m_value.ToString(m_timeFormat);
       Rectangle textBounds = new Rectangle(10, 0, this.Width - 40, this.Height);
       TextRenderer.DrawText(g, displayText, this.Font, textBounds, currentTextColor, TextFormatFlags.VerticalCenter | TextFormatFlags.Left);
 
